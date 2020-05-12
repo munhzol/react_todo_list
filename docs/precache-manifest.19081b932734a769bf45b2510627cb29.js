f8bb252a647592c54a5746e7b3aedcc58f1a2dab
munhzol <munhzol@gmail.com>
2020-05-12 01:31:21 -0400

    second

---
+++ docs/precache-manifest.19081b932734a769bf45b2510627cb29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f91b0f5351735419969ef6457cfb1c8",
+    "url": "/react_todo_list/index.html"
+  },
+  {
+    "revision": "b218dab4a888b528e970",
+    "url": "/react_todo_list/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9bbed67eb7ef50b613cf",
+    "url": "/react_todo_list/static/css/main.9040ac2a.chunk.css"
+  },
+  {
+    "revision": "b218dab4a888b528e970",
+    "url": "/react_todo_list/static/js/2.79912a26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_todo_list/static/js/2.79912a26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bbed67eb7ef50b613cf",
+    "url": "/react_todo_list/static/js/main.c57c64be.chunk.js"
+  },
+  {
+    "revision": "a84f2c38458018baeae0",
+    "url": "/react_todo_list/static/js/runtime-main.062c0b5b.js"
+  }
+]);
